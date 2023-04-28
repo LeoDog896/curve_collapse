8d0fd861713614c3b99c7d76d090f8c6bc95d2a7
Tristan F <LeoDog896@hotmail.com>
2023-04-28 15:15:41 +0000

    feat: base (broken!) implementation

--- leo.test.ts
+++ leo.test.ts
@@ -1,14 +1,24 @@
-import { slope, integrate } from "./leo.ts";
+import { getUpperBound, slope, makePoints, integral } from "./leo.ts";
 import { assertAlmostEquals } from "https://deno.land/std@0.183.0/testing/asserts.ts";
 
 Deno.test("slope", () => {
-    const f = (x: number) => x;
-    const s = slope(f, 0);
-    assertAlmostEquals(s, 1);
+  const f = (x: number) => x;
+  const s = slope(f, 0);
+  assertAlmostEquals(s, 1);
 });
 
-Deno.test("integrate", () => {
-    const f = (x: number) => x;
-    const i = integrate(0, 1, f);
-    assertAlmostEquals(i, 0.5);
+Deno.test("upper bound", () => {
+  const f = (x: number) => x;
+  const b = getUpperBound(f, 0, 0.5);
+  assertAlmostEquals(b, 1);
+});
+
+Deno.test("integral", () => {
+  const f = (x: number) => x;
+  const i = integral(f, 0, 1);
+  assertAlmostEquals(i, 0.5);
+});
+
+Deno.test("make points", () => {
+  console.log(makePoints((x) => x, 0, 1, 10));
 });
--- leo.ts
+++ leo.ts
@@ -1,34 +1,69 @@
 type Func = (x: number) => number;
 
 // get the slope of a function at a point (derivative)
-export function slope(f: Func, x: number, dx = Math.pow(10, -10)): number {
+export function slope(f: Func, x: number, dx = 10 ** -10): number {
   return (f(x + dx) - f(x)) / dx;
 }
 
-// get the integral of a function from a to b
-export function integrate(a: number, b: number, f: Func, dx = Math.pow(10, -3)) {
-  // calculate the number of trapezoids
-  const n = (b - a) / dx;
-
-  // define the variable for area
-  let area = 0;
-
-  //loop to calculate the area of each trapezoid and sum.
-  for (let i = 1; i <= n; i++) {
-    //the x locations of the left and right side of each trapezpoid
-    const x0 = a + (i - 1) * dx;
-    const x1 = a + i * dx;
-
-    // the area of each trapezoid
-    const ai = dx * (f(x0) + f(x1)) / 2.;
-
-    // cumulatively sum the area
-    area += ai;
-  }
-  return area;
+// get the trapezoidal area under a curve
+export function TRAM(
+  a: number,
+  f: Func,
+  dx = 10 ** -3,
+) {
+  return dx * (f(a) + f(a + dx)) / 2;
 }
 
-// get the distance of a function from a to b
+// get the integral of a function
+export function integral(
+  f: Func,
+  a: number,
+  b: number,
+  dx = 10 ** -3,
+) {
+  let sum = 0;
+  for (let i = a; i < b; i += dx) {
+    sum += TRAM(i, f, dx);
+  }
+  return sum;
+}
+
+// gets the upper bound of an integral
+// where ing(f, a, b) = d, find b.
+export function getUpperBound(f: Func, a: number, d: number, dx = 10 ** -3): number {
+  let dxCount = 0;
+  let sum = TRAM(a, f, dx);
+  while (sum < d) {
+    sum += TRAM(a + dxCount * dx, f, dx);
+    dxCount++;
+  }
+
+  return a + dxCount * dx;
+}
+
+// generates a function that returns the distance travelled along a path
 function distance(f: Func): Func {
-    return x => Math.sqrt(1 + Math.pow(slope(f, x), 2));
+  return (x) => Math.sqrt(1 + Math.pow(slope(f, x), 2));
+}
+
+// make equidistant points
+export function makePoints(f: Func, a: number, b: number, n: number, dx = 10 ** -3): [number, number][] {
+  // first, get the total distance travelled from a to b.
+  const d = integral(distance(f), a, b, dx);
+
+  // we can then divide this to make "distance steps" to count when we reverse d to get the bounds of each step.
+  const step = d / n;
+
+  // now, we can make the points.
+  const points: [number, number][] = [];
+
+  // we start at a, and then we add the step to it until we reach b.
+  for (let i = a; i < b; i += step) {
+    // we then get the upper bound of the integral of distance from a to b, where the distance is equal to the step.
+    const b = getUpperBound(distance(f), i, step, dx);
+    // we then add the point to the array.
+    points.push([b, f(b)]);
+  }
+
+  return points;
 }
